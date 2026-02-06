6468197b68258e7ef84c736ee6ea8254401eafb9
agashevishwas189-creator <agashevishwas189@gmail.com>
2026-02-06 22:12:59 +0530

    n

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -13,14 +13,14 @@ resource "aws_security_group" "web_sg" {
     vpc_id = data.aws_vpc.default.id
 
     ingress {
-        from_port = "80"
-        to_port = "80"
+        from_port = 80
+        to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-    ingress  {
+    ingress {
         from_port = 0
-        to_port = 0
+        to_port = 0 
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
@@ -36,7 +36,7 @@ resource "aws_alb" "app_lb" {
     internal = false
     load_balancer_type = "application"
     security_groups = [aws_security_group.web_sg.id]
-    subnets = data.aws_subnets.default.id 
+    subnets = data.aws_subnets.default.ids 
 }
 resource "aws_lb_target_group" "app_tg" {
     name = "app-tg"
@@ -56,7 +56,7 @@ resource "aws_lb_listener" "app_listener" {
 }
 
 resource "aws_launch_template" "example" {
-    name_prefix   = "foobar"
+    name_prefix   = "simple-template-"
     image_id      = var.ami_webserver
     instance_type = var.webserver_type
 
@@ -64,8 +64,7 @@ resource "aws_launch_template" "example" {
         associate_public_ip_address = true
         security_groups = [aws_security_group.web_sg.id]
     }
-}
- user_data = base64decode(<<-EOF
+    user_data = base64encode(<<-EOF
                 #!/bin/bash
                 sudo yum update -y
                 sudo yum install httpd -y
@@ -83,7 +82,7 @@ resource "aws_auto_scaling_group" "asg" {
     vpc_zone_identifier = data.aws_subnets.default.ids
     target_group_arns = [aws_lb_target_group.app_tg.arn]
     health_check_type = "ELB"
-    health_check_grace_period = 100
+    health_check_grace_period = 120
     
         launch_template {
         id = aws_launch_template.example.id
@@ -92,4 +91,3 @@ resource "aws_auto_scaling_group" "asg" {
     
   
 }
-
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "output_webserver_sg" {
     value = aws_security_group.web_sg.id
 }
+output "alb_dns_name" {
+    value = aws_alb.app_lb.dns_name
+  
+}
