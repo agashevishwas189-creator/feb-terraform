2158cb58fafa9582a51031cd71d76cd48a6b49e7
vishwasagashe <agashevishwas189@gmail.com>
2026-02-06 21:30:43 +0530

    m

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -2,7 +2,7 @@
 data "aws_vpc" "default" {
     default = true
 }
-data "aws_subnets" "default" {
+data "aws_subnet" "default" {
     filter {
         name = "vpc-id"
         values = [data.aws_vpc.default.id]
@@ -36,7 +36,7 @@ resource "aws_alb" "app_lb" {
     internal = false
     load_balancer_type = "application"
     security_groups = [aws_security_group.web_sg.id]
-    subnets = data.aws_subnet.default.id 
+    subnets = data.aws_subnets.default.id 
 }
 resource "aws_lb_target_group" "app_tg" {
     name = "app-tg"
@@ -65,3 +65,31 @@ resource "aws_launch_template" "example" {
         security_groups = [aws_security_group.web_sg.id]
     }
 }
+ user_data = base64decode(<<-EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo yum install httpd -y
+                sudo systemctl start httpd
+                sudo systemctl enable httpd
+                echo "<h1>Welcome to Terraform Web Server</h1>" > /var/www/html/index.html
+                EOF
+    )
+}
+resource "aws_auto_scaling_group" "asg" {
+    name = "asg"
+    max_size = 3
+    min_size = 2
+    desired_capacity = 2
+    vpc_zone_identifier = data.aws_subnets.default.ids
+    target_group_arns = [aws_lb_target_group.app_tg.arn]
+    health_check_type = "ELB"
+    health_check_grace_period = 100
+    
+        launch_template {
+        id = aws_launch_template.example.id
+        version = "$Latest"
+    }
+    
+  
+}
+
